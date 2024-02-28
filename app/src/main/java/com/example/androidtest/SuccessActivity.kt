18d98a95b223156e12b5caa009c746d3f9fea4fb
JUST_Nic3 <lukakotorac05@gmail.com>
2024-02-28 15:09:21 +0100

    OnCreate function

--- app/src/main/java/com/example/androidtest/SuccessActivity.kt
+++ app/src/main/java/com/example/androidtest/SuccessActivity.kt
@@ -2,10 +2,15 @@ package com.example.androidtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class SuccessActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_success)
+        val name: String? = intent.getStringExtra("name")
+        val message = "Good job $name! You made 10 steps"
+        val textShare = findViewById<TextView>(R.id.textShare)
+        textShare.text = message
     }
 }
