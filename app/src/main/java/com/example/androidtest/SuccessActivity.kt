b5422eb67c33da26e71076037f85683c737abb8b
JUST_Nic3 <lukakotorac05@gmail.com>
2024-02-28 15:09:56 +0100

    Finished activity

--- app/src/main/java/com/example/androidtest/SuccessActivity.kt
+++ app/src/main/java/com/example/androidtest/SuccessActivity.kt
@@ -1,8 +1,13 @@
 package com.example.androidtest
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.SharedPreferences
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class SuccessActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,4 +18,15 @@ class SuccessActivity : AppCompatActivity() {
         val textShare = findViewById<TextView>(R.id.textShare)
         textShare.text = message
     }
+    fun setOnClickListenerShare(view: View) {
+        val name: String? = intent.getStringExtra("name")
+        val smsNumber: String = "0953594946"
+        val smsText: String = "Good job $name! You made 10 steps"
+        val uri = Uri.parse("smsto:$smsNumber")
+        val intent = Intent(Intent.ACTION_SENDTO, uri)
+        intent.putExtra("sms_body", smsText)
+        startActivity(intent)
+    }
+
+
 }
