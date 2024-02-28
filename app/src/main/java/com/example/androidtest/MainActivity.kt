fa43789de70e695a404b03a2f78ef161fc9eb116
JUST_Nic3 <lukakotorac05@gmail.com>
2024-02-28 14:56:55 +0100

    OnCreate_function

--- app/src/main/java/com/example/androidtest/MainActivity.kt
+++ app/src/main/java/com/example/androidtest/MainActivity.kt
@@ -5,7 +5,13 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main)
+        //Toast.makeText(applicationContext, "onCreate", Toast.LENGTH_SHORT).show()
+        /*Log.i("MyLog", "onCreate")
+        Log.d("My Log D", "onCreate")
+        Log.v("My Log V", "onCreate")
+        Log.w("My Log V", "onCreate")
+        Log.e("My Log V", "onCreate")*/
     }
 }
